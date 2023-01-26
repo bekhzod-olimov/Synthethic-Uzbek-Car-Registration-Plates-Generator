07a2541bdf827a2aed35a5118316c2ce22f822dc
Bekhzod Olimov <50166164+bekhzod-olimov@users.noreply.github.com>
2023-01-26 11:09:07 +0900

    Update generate.py

--- generate.py
+++ generate.py
@@ -1,5 +1,5 @@
-import argparse, os
 from plate_generator import PlateGenerator
+import argparse, os
 import pandas as pd
 
 def run(args):
@@ -18,6 +18,7 @@ def run(args):
         df = pd.read_csv(args.data_path)
         texts = [os.path.basename(filename) for filename in df["filename"]]
         generator = PlateGenerator(save_path=args.save_path, random=args.random)    
+        
         for text in texts:
             split_and_generate(generator, text, args.save, 1)    
 
@@ -30,8 +31,9 @@ if __name__ == "__main__":
     parser.add_argument("-np", "--number_of_plates", help = "Number of images to generate", type = int, default = 3)
     parser.add_argument("-s", "--save", help = "Saving option", type = bool, default = True)
     parser.add_argument("-r", "--random", help = "Generate random plate numbers", type = bool, default = True)
-    parser.add_argument("-sl", "--sample", help = "Sample plate number to distinguish state or long plate numbers", type = str, default = "01227AAA")
-    # parser.add_argument("-sl", "--sample", help = "Sample plate number to distinguish state or long plate numbers", type = str, default = "01A227AA")
+    # parser.add_argument("-sl", "--sample", help = "Sample plate number to distinguish basic, state, foreign plate numbers", type = str, default = "01227AAA")
+    # parser.add_argument("-sl", "--sample", help = "Sample plate number to distinguish basic, state, foreign plate numbers", type = str, default = "01A227AA")
+    parser.add_argument("-sl", "--sample", help = "Sample plate number to distinguish basic, state, foreign plate numbers", type = str, default = "01H012345")
     
     args = parser.parse_args()
     run(args) 
