6bdb84fd20f63ae836bfe6ef7b81bae78c64d356
Bekhzod Olimov <50166164+bekhzod-olimov@users.noreply.github.com>
2023-01-31 12:11:56 +0900

    Update generate.py

--- generate.py
+++ generate.py
@@ -5,25 +5,25 @@ import numpy as np
 
 def run(args):
     
+    generator = PlateGenerator(save_path=args.save_path, random=args.random)
+
     if args.random:
-        generator = PlateGenerator(save_path=args.save_path, random=args.random)
         generator.generate(save=args.save, num=args.number_of_plates, plate=None, plate_type=None, region=None)
         
     else:
         df = pd.read_csv(args.data_path)
         texts = [os.path.basename(filename) for filename in df["filename"]]
-        generator = PlateGenerator(save_path=args.save_path, random=args.random)    
         for text in texts:
             generator.generate(text, args.save, num=1, plate_type=None, region=None)
 
 if __name__ == "__main__":
     
-    parser = argparse.ArgumentParser(description="Synthetic Plate Number Generator")
+    parser = argparse.ArgumentParser(description="Synthetic Uzbek Car Registration Plates Generator")
     
     parser.add_argument("-dp", "--data_path", help = "Path to the csv file with plate numbers", type = str, default = "uzbek.csv")
-    parser.add_argument("-sp", "--save_path", help = "Directory to save generated images", type = str, default = "./new_samples/to_test_new/")
+    parser.add_argument("-sp", "--save_path", help = "Directory to save generated images", type = str, default = "./new_samples/aaa/")
     parser.add_argument("-s", "--save", help = "Saving option", type = bool, default = True)
-    parser.add_argument("-np", "--number_of_plates", help = "Number of images to generate", type = int, default = 20)
+    parser.add_argument("-np", "--number_of_plates", help = "Number of images to generate", type = int, default = 10000)
     parser.add_argument("-r", "--random", help = "Generate random plate numbers", type = bool, default = True)
     
     
