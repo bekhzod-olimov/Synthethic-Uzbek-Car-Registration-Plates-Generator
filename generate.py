d6fa36ba30ba0d45cbd058fe18055ce58ee02387
Bekhzod Olimov <50166164+bekhzod-olimov@users.noreply.github.com>
2023-03-02 15:44:06 +0900

    generate script

--- generate.py
+++ generate.py
@@ -16,10 +16,13 @@ def run(args):
     
     """
     
-    generator = PlateGenerator(save_path=args.save_path, random=args.random, transformations=args.transform)
-
+    # Initialize PlateGenerator instance based on the parsed arguments
+    generator = PlateGenerator(save_path = args.save_path, random = args.random, transformations = args.transform)
+    
+    # Random LP Generation
     if args.random: generator.generate(save = args.save, num = args.number_of_plates, plate = None, plate_type = None, region = None)
-        
+
+    # Pre-defined LP Generation    
     else:
         df = open(args.data_path, 'r')
         for idx, text in enumerate(df):
@@ -29,8 +32,10 @@ def run(args):
 
 if __name__ == "__main__":
     
+    # Initialize Argument Parser
     parser = argparse.ArgumentParser(description="Synthetic Uzbek Car Registration Plates Generator")
     
+    # Add arguments to the parser
     parser.add_argument("-dp", "--data_path", help = "Path to the csv file with plate numbers", type = str, default = "lp.txt")
     parser.add_argument("-sp", "--save_path", help = "Directory to save generated images", type = str, default = "./new_samples/test/")
     parser.add_argument("-s", "--save", help = "Saving option", type = bool, default = True)
@@ -38,6 +43,8 @@ if __name__ == "__main__":
     parser.add_argument("-r", "--random", help = "Generate random plate numbers", type = bool, default = False)
     parser.add_argument("-t", "--transform", help = "Apply transformations", type = bool, default = True)
     
-    
+    # Parse the arguments
     args = parser.parse_args()
+    
+    # Run the code using the parsed arguments
     run(args)
