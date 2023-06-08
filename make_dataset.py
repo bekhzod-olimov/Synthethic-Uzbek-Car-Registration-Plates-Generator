5d7f283df80ff803b41a28f22d55ee56c053c93f
Bekhzod Olimov <50166164+bekhzod-olimov@users.noreply.github.com>
2023-06-08 10:48:37 +0900

    script to create datasets for training

--- make_dataset.py
+++ make_dataset.py
@@ -22,10 +22,10 @@ def copy_files(im_paths, destination):
     """
     This function gets image paths and destination and copies the images to the destination.
     
-    Arguments:
+    Parameters:
     
-        im_paths    - paths to the images from domain A and B;
-        destination - path to the directory to copy the images.
+        im_paths    - paths to the images from domain A and B, str;
+        destination - path to the directory to copy the images, str.
         
     Output:
         
@@ -33,14 +33,12 @@ def copy_files(im_paths, destination):
     
     """
     
-    for file in im_paths:
-        shutil.copy(file, destination)
+    for file in im_paths: shutil.copy(file, destination)
         
 def get_train_ims(ims_paths, im_files): return sorted(glob(f"{ims_paths}/*/*{[im_file for im_file in im_files]}"))
 def get_test_ims(ims_paths, im_files): return sorted(glob(f"{ims_paths}/*{[im_file for im_file in im_files]}"))
 
-for arg in vars(args):
-    print('[%s] = ' % arg, getattr(args, arg))
+for arg in vars(args): print('[%s] = ' % arg, getattr(args, arg))
 
 # Initialize a list with proper image filetypes 
 im_files = [".jpg", ".png", ".jpeg"]
@@ -58,11 +56,9 @@ print(f"There are {len(output_im_paths)} original images!")
 num_ims = min(args.num_ims, len(input_im_paths))
 
 # Create directories to copy the train and test images
-os.makedirs(args.trainA, exist_ok=True)
-os.makedirs(args.trainB, exist_ok=True)
+os.makedirs(args.trainA, exist_ok = True); os.makedirs(args.trainB, exist_ok = True)
 print("Copying images...")
 
 # Copy the images
-copy_files(input_im_paths, args.trainA)
-copy_files(output_im_paths, args.trainB)
+copy_files(input_im_paths, args.trainA); copy_files(output_im_paths, args.trainB)
 print("Done!")
